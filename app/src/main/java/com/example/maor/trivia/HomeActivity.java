edb15d928d7f03f52f18dd04495de03f74283e54
maormaor153153 <maormaor153@gmail.com>
2018-02-17 15:18:23 +0200

    The first One for check

--- app/src/main/java/com/example/maor/trivia/HomeActivity.java
+++ app/src/main/java/com/example/maor/trivia/HomeActivity.java
@@ -3,12 +3,13 @@ package com.example.maor.trivia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //hello
         setContentView(R.layout.activity_main);
+
 
     }
 }
